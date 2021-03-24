1b644c6b145b98db58170b7feaec67226b1d9313
emmy <emmy66418@gmail.com>
2021-03-24 14:46:20 +0200

    Add user interface for players

--- bin/main.rb
+++ bin/main.rb
@@ -12,11 +12,39 @@ player_name2 = gets.chomp
 
 puts "#{player_name1} will play with x and #{player_name2} will play with o"
 
-puts "Enter a number between 1 and 9"
-number = gets.chomp.to_i
+puts "Let's start the game"
 
-if number < 1 || number > 9 then
-    puts "Invalid number"
-else
-    puts number
-end
+puts "Enter a number between 1 and 9"
+user_input = gets.chomp
+
+def input_to_index(user_input)
+    user_input.to_i - 1
+ end
+
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
+ display_board(board)
+
+
+ def valid_move?(input)
+    input.between?(0, 8) && !position_token?(input)
+  end
+
+
+  def turn
+    puts "Choose a spot between 1-9"
+    spot = gets.strip
+    spot = input_to_index(spot)
+    if valid_move?(spot)
+      move(spot, current_player)
+    else
+      turn
+    end
+    display_board
+  end
