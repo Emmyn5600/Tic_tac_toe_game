80cbdc37ccf743a2bc55ea92f92c2ac08003a355
emmy <emmy66418@gmail.com>
2021-03-24 19:54:30 +0200

    Add more definition for the  methods for our game project

--- bin/main.rb
+++ bin/main.rb
@@ -1,88 +1,117 @@
 #!/usr/bin/env ruby
 
-require_relative '../lib/helper.rb'
-require_relative '../lib/player.rb'
+require_relative '../lib/player'
+require_relative '../lib/helper'
 
 class TicTacToe
+  def initialize
+    @_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @name = []
+    @helper = Helper.new
+    @game_over = false
+    @counter = 0
+  end
 
-    def initialize
-        @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-        @name = []
-        @helper = Helpers.new
-        @game_over = false
-        @counter = 0
-    end    
+  def start
+    puts 'Welcome to TIC-TAC-TOE'
+    puts ''
+  end
 
-    def begin
-        puts "Welcome to TIC-TAC-TOE"
-        puts " "
-        sleep 1
-        puts "A simple game developed by Tiago and Emmy"
-        puts " "
-        sleep 1
+  def display_board
+    puts "  #{@_board[0]}, #{@_board[1]}, #{@_board[2]}"
+    puts separator = '-----------'
+    puts "  #{@_board[3]}, #{@_board[4]}, #{@_board[5]}"
+    puts separator
+    puts "  #{@_board[6]}, #{@_board[7]}, #{@_board[8]}"
+    puts ''
+  end
+
+  def player_move(user_input)
+    user_input.to_i - 1
+  end
+
+  def move_on_board(index)
+    @_board[index] = @player.token
+  end
+
+  def turn_count
+    if @_board.include?('X')
+      @counter += 1
+    elsif @_board.include?('O')
+      @counter += 1
+    else
+      @counter
+    end
+    @counter
+  end
+
+  def turn
+    @player = @player == @name[0] ? @name[1] : @name[0]
+    puts "#{@player.name}, choose a spot between 1-9"
+    player_play = gets.strip.to_i
+    until player_play.positive?
+      puts 'Please enter a valid move'
+      player_play = gets.strip.to_i
+    end
+    until @helper.valid_move?(@_board, player_move(player_play))
+      puts "#{@player.name}, Invalid number. Choose another number between 1-9"
+      player_play = gets.strip.to_i
+    end
+    move_on_board(player_move(player_play))
+    display_board
+    if @helper.won?(@_board)
+      puts "Congratulations #{@player.name} you won!!!"
+      @game_over = true
+    elsif @helper.draw?(turn_count)
+      puts 'It is a Draw!!!'
+      @game_over = true
+    end
+  end
+
+  def names_of_users
+    tokens = %w[X O]
+
+    2.times do |item|
+      puts 'Please enter names of players: '
+      player_name = gets.chomp.strip
+      puts ''
+
+      while player_name.empty?
+        puts 'Please enter a valid name'
+        player_name = gets.strip.to_s
+        break unless player_name.empty?
+      end
+      @name << Player.new(player_name, tokens[item])
     end
 
-    def display_board(board)
-        puts "\n #{board[0]} | #{board[1]} | #{board[2]} "
-        puts '-----------'
-        puts " #{board[3]} | #{board[4]} | #{board[5]} "
-        puts '-----------'
-        puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    until @name[1].name != @name[0].name
+      puts "Your name should be different than #{@name[0].name}:"
+      player2 = gets.strip.to_s
+      @name[1] = Player.new(player2, 'O')
     end
+    sleep 1
+  end
 
-    def player_move(user_input)
-        user_input.to_i - 1
-    end
+  def greeting
+    puts "Today our players are: #{@name[0].name} and #{@name[1].name}"
+    puts ''
+    sleep 1
+    puts "#{@name[0].name} is going to play with 'X' symbol."
+    sleep 1
+    puts ''
+    puts "#{@name[1].name} is going to play with 'O'symbol"
+    sleep 1
+  end
 
-    def move_on_board(index)
-        @_board[index] = @player.token
-    end
-
-    def turn_count
-        if @_board.include?('X')
-            @counter += 1
-        elsif @_board.include?('O')
-            @counter += 1
-        else
-            @counter
-        end
-        @counter
-    end  
+  def execute
+    start
+    system('clear')
+    names_of_users
+    greeting
+    puts ''
+    turn until @game_over
+  end
 end
 
-
-
-
-
-
-=begin
-puts display.display_name_prompt(1)
-player1 = gets.chomp.capitalize
-puts display.display_name_prompt(2)
-
-puts "\n#{player1} will play with x and #{player2} will play with o"
-puts "\nLet's start the game"
-puts "\nChoose the letters between 1 and 9 to place your mark on the board"
-
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-show_board = Board.new 
-show_board.display_board(board)
-
-puts "\n#{player1} Enter a number between 1 and 9"
-user_input1 = gets.chomp
-
-case user_input1.to_i
-when 1 then puts 'You are the winner!'
-when 2 then puts 'It is a tie!'
-end
-
-display_board(board)
-puts "\n#{player2} Enter a number between 1 and 9"
-user_input2 = gets.chomp
-
-case user_input2.to_i
-when 1 then puts 'You are the winner!'
-when 2 then puts 'It is a tie!'
-end
-
-=end
+play_game = TicTacToe.new
+play_game.execute
--- lib/helper.rb
+++ lib/helper.rb
@@ -11,12 +11,16 @@ class Helper
     ]
 
     def position_taken?(board, index)
-    (board[index] == (board[index] == '') || (board[index] == ' ') || board[index].nil?) ? false : true
-     end
-
-     def valid_move?(board, index)
+        if board[index] == (board[index] == '') || (board[index] == ' ') || board[index].nil?
+          false
+        else
+          true
+        end
+      end
+    
+      def valid_move?(board, index)
         index.between?(0, 8) && !position_taken?(board, index)
-     end
+      end
 
      def won?(board)
         WINNING_CASES.find do |item|
