23a5e090c424a634b46464ab0d3e356bda2cd7d0
emmy <emmy66418@gmail.com>
2021-03-25 17:21:05 +0200

    Fix the issues

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,5 +1,5 @@
 class Helper
-  WINNING_CASES = [
+  WINNING_CASES =[
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -8,7 +8,7 @@ class Helper
     [2, 5, 8],
     [6, 4, 2],
     [0, 4, 8]
-  ].freeze
+  ]
 
   def position_taken?(board, index)
     !(board[index] == (board[index] == '') || (board[index] == ' ') || board[index].nil?)
