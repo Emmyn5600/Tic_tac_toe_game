97f565e161f48530da3b66ce5c4647adc95c8206
emmy <emmy66418@gmail.com>
2021-03-25 17:13:55 +0200

    Work on the issues

--- bin/main.rb
+++ bin/main.rb
@@ -44,11 +44,11 @@ class TicTacToe
   end
 
   def show_board
-    puts "  #{@board[0]}, #{@board[1]}, #{@board[2]}"
+    puts "  #{@board[0]} | #{@board[1]} | #{@board[2]}"
     puts separator = '-----------'
-    puts "  #{@board[3]}, #{@board[4]}, #{@board[5]}"
+    puts "  #{@board[3]} | #{@board[4]} | #{@board[5]}"
     puts separator
-    puts "  #{@board[6]}, #{@board[7]}, #{@board[8]}"
+    puts "  #{@board[6]} | #{@board[7]} | #{@board[8]}"
     puts ''
   end
 
